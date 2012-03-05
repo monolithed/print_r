15570e7b29ea88f2a4f27c6731b373007dae4e7a
Alexander Guinness <monolithed@gmail.com>
2012-03-06 01:47:39 +0500

    v1.1

--- Python version/README.md
+++ Python version/README.md
@@ -5,6 +5,37 @@ This module provides a function that prints human-readable information about the
 ##Prototype:
 	string print_r (mixed object [, bool view = False])
 
+##Installation
+
+*1. Install using [Setuptools] (http://pypi.python.org/pypi/setuptools):*
+
+Download the module from: http://pypi.python.org/pypi/pyprint_r/
+
+	# python setup.py install
+
+Or run using [easy_install] (http://packages.python.org/distribute/easy_install.html):
+
+	# easy_install pyprint_r
+
+*Include:*
+
+	from pyprint_r import print_r
+
+NOTE: If you have not installed [setuptools] (http://pypi.python.org/pypi/setuptools) you should read about [Distribute] (http://packages.python.org/distribute/).
+
+*Distribute installation:*
+
+	curl -O http://python-distribute.org/distribute_setup.py
+	python distribute_setup.py
+	easy_install pip
+
+*2. Install using the compilation:*
+Create a *.py* file and include:
+
+	from print_r import *
+
+Then you will get a file named *print_r.pyc* and place the one in your installation/working directory
+
 ## Use
 *For example you could use the following code:*
 
@@ -12,7 +43,7 @@ This module provides a function that prints human-readable information about the
 
 	class Class:
 		def __init__(self):
-			self.foo = 10
+			self.foo = 1
 
 	instance = Class()
 
@@ -22,11 +53,10 @@ This module provides a function that prints human-readable information about the
 			'string': "text",
 			'list' : [1, 2, 3]
 		},
-		'class' : Class,
 		'instance' : instance
 	};
 
-	print_r(Class);
+	print_r(instance);
 	print_r(obj);
 	print_r(list);
 
@@ -34,18 +64,23 @@ This module provides a function that prints human-readable information about the
 
 	class
 	------------------------------
-	<class '__main__.Class'>
+	instance: {
+		foo: 1
+	},
 
 	dict
 	------------------------------
 	{
-		class: <class '__main__.Class'>,
 		instance: {
 			foo: 10
 		},
 		number: 1,
 		object: {
-			list: [1, 2, 3],
+			'list' : [
+				[0] => 1,
+				[1] => 2,
+				[2] => 3
+			],
 			string: "text"
 		}
 	}
@@ -67,13 +102,16 @@ If you set the second parameter <view> in boolean value True, you will get an al
 	dict
 	------------------------------
 	{
-		[class] => <class '__main__.Class'>,
 		[instance] => {
-			[foo] => 10
+			[foo] => 1
 		},
 		[number] => 1,
 		[object] => {
-			[list] => [1, 2, 3],
+			[list] : [
+				[0] => 1,
+				[1] => 2,
+				[2] => 3
+			]
 			[string] => "text"
 		}
 	}
--- Python version/print_r.py
+++ Python version/print_r.py
@@ -3,46 +3,78 @@
 - string print_r (mixed object [, bool view = False])
 - Prints human-readable information about the object
 - @author: Alexander Guinness
-- @version: 1.0
+- @version: 1.1
 - @params: {mixed} The Object to be printed
 - @params: {view} Optional boolean parameter to set an alternative view
 - @return string represented by standard output function print()
-- @license: MIT
+- @license: PSF, MIT
 - @date: 2/27/12 9:28 PM
 '''
 __all__ = ["print_r"]
 
-def print_r(obj, view = 0):
-	def build(obj, indent = ''):
-		data = []
+class main:
+	def __init__(self, data, view):
+		'''
+		- __init__ (mixed data [, bool view = False])
+		'''
+		self.data = data
+		self.view = view;
 
-		if isinstance(obj, list) and not indent:
-			for key, value in enumerate(obj):
-				data.extend(['[', key, '] => ', value, '\n'])
+	def __str__(self):
+		return self.build(self.data)
+
+	def build(self, data, indent = ''):
+		'''
+		- string build (mixed data [, str indent = ''])
+		'''
+		output = []
+
+		def get_depth(item):
+			return self.set_depth(item, output, indent)
+
+		#dict
+		if isinstance(data, dict):
+			get_depth(data.items())
+
+		#list
+		elif isinstance(data, list):
+			get_depth(enumerate(data))
+
+		#str
+		elif isinstance(data, str):
+			output.extend(['"', data, '"'])
+
+		#instance
+		elif '__dict__' in dir(data) and type(data) is not type:
+			get_depth(data.__dict__.items())
+
+		#other
 		else:
-			def depth(item):
-				def get_view(i):
-					return ''.join(view and ['\t[', i, '] => '] or ['\t', i, ': '])
+			output.append(data)
 
-				block = ['{\n']
+		return ''.join(map(str, output))
 
-				for key, value in sorted(item):
-					block.extend([indent, get_view(key), build(value, indent + '\t'), ',', '\n'])
+	def set_depth(self, item, output, indent):
+		'''
+		- void set_depth (mixed item, dict output, str indent)
+		'''
+		is_list = isinstance(item, enumerate) and 1 or 0
 
-				block.pop(-2)
-				data.extend([''.join(block), indent, '}'])
+		brace = [['{', '}'], ['[', ']']][is_list]
+		block = [brace[0], '\n']
 
-			if isinstance(obj, dict):
-				depth(obj.items())
+		for key, value in sorted(item):
+			block.extend([indent, self.get_view(key, is_list), self.build(value, indent + '\t'), ',', '\n'])
 
-			elif '__dict__' in dir(obj) and type(obj) is not type:
-				depth(obj.__dict__.items())
+		block.pop(-2)
+		output.extend([''.join(block), indent, brace[1]])
 
-			elif isinstance(obj, str):
-				data.extend(['"', obj, '"'])
+	def get_view(self, key, is_list):
+		'''
+		- string get_view (mixed key, bool is_list)
+		'''
+		return ''.join(map(str, (self.view or is_list) and ['\t[', key, '] => '] or ['\t', key, ': ']))
 
-			else:
-				data.append(obj)
 
-		return ''.join(map(str, data))
-	print(build(obj))
+def print_r(data, view = 0):
+	return print(main(data, view))
