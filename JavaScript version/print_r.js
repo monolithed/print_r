19925e10f3fe2c64e5df41c5543926da50354056
Alexander Guinness <monolithed@gmail.com>
2012-03-07 12:00:44 +0500

    README: added description

--- JavaScript version/print_r.js
+++ JavaScript version/print_r.js
@@ -4,14 +4,16 @@
  * Prints human-readable information about the object
  * @author: Alexander Guinness
  * @version: 1.1
- * @params: {mixed} The Object to be printed
- * @params: {view} Optional boolean parameter to set an alternative view
+ * @params: {mixed} data - the Object to be printed
+ * @params: {boolean} view - optional boolean parameter to set an alternative view
  * @return String
  * @license: MIT
  * @date: 2/27/12 9:28 PM
 **/
 
 var print_r = function(data, view) {
+	'use strict'
+
 	/*
 	- string build (mixed data [, string indent = ''])
 	*/
@@ -27,7 +29,7 @@ var print_r = function(data, view) {
 				else
 					this.output.push({
 						'string':   '"' + data + '"',
-						'function': data.toString().replace(/\b.*\n|\}/g, '\t$&').replace(/^\t/, ' ')
+						'function': data.toString().replace(/\b.*\n|\}$/g, '\t$&').replace(/^\t/, ' ')
 					}[this.type(data)] || data);
 
 				return this.output.join('');
